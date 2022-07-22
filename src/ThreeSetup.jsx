925b7a1a81ea5118541168160ac282b64894fd21
BpaYWH <yauwinghongbpa@gmail.com>
2022-07-22 17:00:54 +0800

    UPDATE: add back camera view & skeleton renderer

--- src/ThreeSetup.jsx
+++ src/ThreeSetup.jsx
@@ -18,11 +18,13 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { VRMSchema } from "@pixiv/three-vrm";
 import { VRM } from "@pixiv/three-vrm";
 import { VRMUtils } from "@pixiv/three-vrm";
-import { Holistic } from "@mediapipe/holistic";
+import { Holistic, POSE_CONNECTIONS, FACEMESH_TESSELATION, HAND_CONNECTIONS } from "@mediapipe/holistic";
+import { drawLandmarks, drawConnectors } from "@mediapipe/drawing_utils";
 import { Camera } from "@mediapipe/camera_utils";
 
 export default function ThreeSetup() {
    const videoRef = useRef(null);
+   const skeletonRef = useRef(null);
    const vrmRef = useRef(null);
 
    const [isStart, setIsStart] = useState(false);
@@ -384,7 +386,71 @@ export default function ThreeSetup() {
          }
       };
 
+      const drawResults = (results) => {
+         guideCanvas.width = videoElement.videoWidth;
+         guideCanvas.height = videoElement.videoHeight;
+         let canvasCtx = guideCanvas.getContext("2d");
+         canvasCtx.save();
+         canvasCtx.clearRect(0, 0, guideCanvas.width, guideCanvas.height);
+         // Use `Mediapipe` drawing functions
+         drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+            color: "#00cff7",
+            lineWidth: 4,
+         });
+         drawLandmarks(canvasCtx, results.poseLandmarks, {
+            color: "#ff0364",
+            lineWidth: 2,
+         });
+         drawConnectors(
+            canvasCtx,
+            results.faceLandmarks,
+            FACEMESH_TESSELATION,
+            {
+               color: "#C0C0C070",
+               lineWidth: 1,
+            }
+         );
+         if (results.faceLandmarks && results.faceLandmarks.length === 478) {
+            //draw pupils
+            drawLandmarks(
+               canvasCtx,
+               [results.faceLandmarks[468], results.faceLandmarks[468 + 5]],
+               {
+                  color: "#ffe603",
+                  lineWidth: 2,
+               }
+            );
+         }
+         drawConnectors(
+            canvasCtx,
+            results.leftHandLandmarks,
+            HAND_CONNECTIONS,
+            {
+               color: "#eb1064",
+               lineWidth: 5,
+            }
+         );
+         drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+            color: "#00cff7",
+            lineWidth: 2,
+         });
+         drawConnectors(
+            canvasCtx,
+            results.rightHandLandmarks,
+            HAND_CONNECTIONS,
+            {
+               color: "#22c3e3",
+               lineWidth: 5,
+            }
+         );
+         drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+            color: "#ff0364",
+            lineWidth: 2,
+         });
+      };
+
       const onResults = (results) => {
+         drawResults(results);
          animateVRM(currentVrm, results);
       };
 
@@ -404,6 +470,7 @@ export default function ThreeSetup() {
       holistic.onResults(onResults);
 
       let videoElement = videoRef.current;
+      let guideCanvas = skeletonRef.current;
       setWebCamera(
          new Camera(videoElement, {
             onFrame: async () => {
@@ -422,7 +489,8 @@ export default function ThreeSetup() {
 
    return (
       <>
-         <video ref={videoRef} className="input_video" hidden />
+         <video ref={videoRef} className="input_video" />
+         <canvas ref={skeletonRef} />
          <button onClick={setTracking}>{isStart ? "Stop" : "Start"}</button>
          <div className="absoluteDiv">
             <div ref={vrmRef} className="vrm" />
